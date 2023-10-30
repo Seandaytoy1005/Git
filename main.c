3fe5e1ba5b078ea0fffedb93340d073d046248e4
t.se1 <xz10052001@gmail.com>
2023-10-31 03:00:40 +0800

    delete ...

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main(int argc, char **argv)
  fgets(first, 255, stdin);
  first[strlen(first)-1] = '\0'; /* remove the newline at the end */
  printf("Now enter your last name: ");
- gets(last); /* buffer overflow? what's that? */
+ gets(last);
  printf("Hello,%s %s!\n", first, last);
  return 0;
 }
